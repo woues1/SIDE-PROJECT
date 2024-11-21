2d400e15ea3baed2645e78504997807f97d66a27
woues1 <toni.hirvikallio@gmail.com>
2024-11-21 20:47:47 +0200

    create admin Schema

---
+++ Backend/models/adminModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const adminSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    JWT: {
+        type: String,
+    },
+
+}, {timestamps: true})
+
+module.exports = mongoose.model('Admin', adminSchema);
