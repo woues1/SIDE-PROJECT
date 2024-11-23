e2ef8c0a7f8637fa0626364ad64a232cf6216e2b
woues1 <toni.hirvikallio@gmail.com>
2024-11-23 19:15:48 +0200

    changes to config

--- Frontend/src/components/Projects.tsx
+++ Frontend/src/components/Projects.tsx
@@ -12,7 +12,7 @@ function Projects() {
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        const response = await fetch("/api/projects",{
+        const response = await fetch("/api/projects", {
           method: 'GET'
         });
         const data = await response.json();
@@ -25,25 +25,27 @@ function Projects() {
   }, []);
 
   return (
-    <div id="#projects" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4 bg-gray-900 w-3/4 mx-auto place-items-center">
-      {projects.map((project: Project, index: number) => (
-        <div
-          key={project._id}
-          className={`bg-white shadow-md rounded-lg p-4 hover:shadow-xl transition-shadow ${index === 0 ? 'md:col-start-2' : ''}`}
-        >
-          <h2 className="text-lg font-semibold mb-2 text-black">{project.name}</h2>
-          <p className="text-gray-600 mb-4">{project.description}</p>
-          <a
-            href={project.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 hover:text-blue-700 underline"
+    <>
+      <div id="projects" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4 bg-gray-900 w-3/4 mx-auto place-items-center">
+        {projects.map((project: Project, index: number) => (
+          <div
+            key={project._id}
+            className={`bg-white shadow-md rounded-lg p-4 hover:shadow-xl transition-shadow ${index === 0 ? 'md:col-start-2' : ''}`}
           >
-            Visit Project
-          </a>
-        </div>
-      ))}
-    </div>
+            <h2 className="text-lg font-semibold mb-2 text-black">{project.name}</h2>
+            <p className="text-gray-600 mb-4">{project.description}</p>
+            <a
+              href={project.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:text-blue-700 underline"
+            >
+              Visit Project
+            </a>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { AuthContextProvider } from './context/AuthContext.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './main.css';
+import App from './App.tsx';
+import { AuthContextProvider } from './context/AuthContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
