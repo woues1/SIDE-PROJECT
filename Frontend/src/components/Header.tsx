3334245592f50ee8a93b9445ff3c055ac3bf1f1b
woues1 <toni.hirvikallio@gmail.com>
2024-11-23 22:11:44 +0200

    updated responsive header menu

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,24 +1,36 @@
+import { useState } from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from "../hooks/useLogout";
 
 function Header() {
     const { user } = useAuthContext()
     const { logout } = useLogout()
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const menuToggle = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     const handleClick = () => {
         logout()
     }
+
     return (
-        <header className="bg-gray-800 text-white p-4 shadow-lg">
-            <div className="container flex md:justify-between md:items-center ml-4 lg:mx-auto">
+        <header className="bg-gray-800 text-white p-4 shadow-lg mb-auto md:mb-0">
+            <div className="container flex justify-between md:items-center ml-4 lg:mx-auto">
 
                 <a href="/" className="text-2xl font-bold hover:text-gray-300">
                     Logo
                 </a>
 
-                <nav className="md:flex space-x-4 md:items-center mt-10 md:mt-0">
+                <button className="md:hidden text-2xl ml-auto mr-4" onClick={menuToggle}>
+                {isMenuOpen ? <span>&#10005;</span> : <span>&#9776;</span>}
+                </button>
 
-                    <ul className="md:flex md:items-center md:z-auto md:static absolute w-full md:w-auto left-0 bg-gray-800
-                                   md:py-0 py-4 pl-4 md:py-0 ">
+                <nav className="md:flex space-x-4 md:items-center mt-10 md:mt-0">
+                    <ul className={`md:flex md:items-center md:z-auto md:static absolute w-full md:w-auto left-0 bg-gray-800
+                                   md:py-0 py-4 pl-4 md:py-0 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500
+                                   ${isMenuOpen ? 'top-[50px] opacity-100' : 'top-[-400px] opacity-0'}`}>
                         <li className="mx-4 my-4 md:my-0">
                             <a href="#projects" className="hover:text-gray-300">Projects</a>
                         </li>
